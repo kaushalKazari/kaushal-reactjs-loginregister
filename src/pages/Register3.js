936234a7d24596670e3e43f6506aa73bace0ba38
kaushal <kaushal.kishor@citixsys.com>
2023-09-18 16:45:20 +0530

    droupdown issue pending, left are fixed

--- src/pages/Register3.js
+++ src/pages/Register3.js
@@ -4,14 +4,14 @@ import swal from 'sweetalert';
 const Register3 = () => {
 
     // Hooks variable Area
-    const [name, setName] = useState("")
-    const [mobile, setMobile] = useState("")
-    const [pincode, setPincode] = useState("")
-    const [flat, setFlat] = useState("")
-    const [area, setArea] = useState("")
-    const [country, setCountry] = useState("")
-    const [landmark, setLandmark] = useState("")
-    const [state, setState] = useState("")
+    // const [name, setName] = useState("")
+    // const [mobile, setMobile] = useState("")
+    // const [pincode, setPincode] = useState("")
+    // const [flat, setFlat] = useState("")
+    // const [area, setArea] = useState("")
+    // const [country, setCountry] = useState("")
+    // const [landmark, setLandmark] = useState("")
+    // const [state, setState] = useState("")
 
 
     const [payload, setPayload] = useState(
@@ -29,34 +29,24 @@ const Register3 = () => {
           }
     )
 
+    const changeValue =(e)=>{
+        setPayload(
+            {
+                "data": {
+                  "name": document.querySelector('#fullname').value,
+                  "mobile": document.querySelector('#mobile').value,
+                  "pincode": document.querySelector('#pincode').value,
+                  "flat": document.querySelector('#flat').value,
+                  "area": document.querySelector('#area').value,
+                  "landmark": document.querySelector('#landmark').value,
+                  "country": 'India',
+                  "state": 'Bihar'
+                }
+              }
+        )
+    }
 
-    function SaveData() {
-        // console.log(name, mobile, pincode, flat, area, country, landmark, state)
-
-        // let data = {
-        //    'name' : name,
-        //     'mobile': mobile,
-        //     'pincode': pincode,
-        //     'flat': flat,
-        //     'area': area,
-        //     'landmark': landmark,
-        //     'country': country,
-        //     'state': state
-        // }
-        // let data = [
-        //     {
-        //         "data": {
-        //           "name": "Pooja",
-        //           "mobile": 987316867112,
-        //           "pincode": 121134,
-        //           "flat": "Pooja",
-        //           "area": "Pooja",
-        //           "landmark": "Near11 Pooja Sani Mandir",
-        //           "country": "India",
-        //           "state": "Bihar"
-        //         }
-        //       }
-        // ]
+    const SaveData = ()=> {
 
         fetch(`http://localhost:1337/api/amezonedatas`, {
             method: "POST",
@@ -69,7 +59,7 @@ const Register3 = () => {
             return res.json()
         }).then((data) => {
             console.log(data)
-            if (data.jwt) {
+            if (data) {
                 // alert("you registerd successfully")
                 swal("Good job!", "you registerd successfully.", "success");
             }
@@ -87,34 +77,34 @@ const Register3 = () => {
                     <div className="row mb-3">
                         <div className="col-md-12 mb-3">
                             <label className="form-label">Full name (First and Last name)</label>
-                            <input type="text" name="name" value={name} onChange={(e) => { setName(e.target.value) }} className="form-control" />
+                            <input type="text" name="name" id="fullname" onChange={(e) => { return changeValue(e) }} className="form-control" />
                         </div>
                         <div className="col-md-12 ">
                             <label className="form-label">Mobile number</label>
-                            <input type="number" name="mobile" value={mobile} onChange={(e) => { setMobile(e.target.value) }} className="form-control" />
+                            <input type="number" name="mobile" id="mobile" onChange={(e) => { return changeValue(e) }} className="form-control" />
                         </div>
                     </div>
                     <form className="row g-3">
                         <div className="col-md-12">
                             <label className="form-label">Pincode</label>
-                            <input type="pincode" placeholder="6-digits(0-9) Pincode" name="pincode" value={pincode} onChange={(e) => { setPincode(e.target.value) }} className="form-control" />
+                            <input type="pincode" id="pincode" placeholder="6-digits(0-9) Pincode" name="pincode" onChange={(e) => { return changeValue(e) }} className="form-control" />
                         </div>
                         <div className="col-md-12">
                             <label className="form-label">Flat, House no., Building, Company, Apartment</label>
-                            <input type="text" name="flat" value={flat} onChange={(e) => { setFlat(e.target.value) }} className="form-control" />
+                            <input type="text" id='flat' name="flat" onChange={(e) => { return changeValue(e) }} className="form-control" />
                         </div>
                         <div className="col-12">
                             <label className="form-label">Area, Street, Sector, Village</label>
-                            <input type="text" name="area" value={area} onChange={(e) => { setArea(e.target.value) }} className="form-control" />
+                            <input type="text" name="area" id="area" onChange={(e) => { return changeValue(e) }} className="form-control" />
                         </div>
                         <div className="col-12">
                             <label className="form-label">Landmark</label>
-                            <input type="text" placeholder="e.g. near apollo hospital" name="landmark" value={landmark} onChange={(e) => { setLandmark(e.target.value) }} className="form-control" />
+                            <input type="text" id="landmark" placeholder="e.g. near apollo hospital" name="landmark" onChange={(e) => { return changeValue(e) }} className="form-control" />
                         </div>
                         <div className="col-md-6">
                             <label className="form-label" >Country/Region</label>
 
-                            <select id="country" className="form-select" name="country" value={country} onChange={(e) => { setCountry(e.target.value) }}>
+                            <select id="country" className="form-select" name="country" onChange={(e) => { return changeValue(e) }}>
                                 <option defaultValue>India</option>
                                 <option>Nepal</option>
                                 <option>United States</option>
@@ -123,8 +113,8 @@ const Register3 = () => {
                             </select>
                         </div>
                         <div className="col-md-6">
-                            <label htmlFor="inputState" className="form-label">State</label>
-                            <select id="inputState" className="form-select" name="state" value={state} onChange={(e) => { setState(e.target.value) }}>
+                            <label className="form-label">State</label>
+                            <select className="form-select" name="state" onChange={(e) => { return changeValue(e) }}>
                                 <option defaultValue>Choose...</option>
                                 <option>Bihar</option>
                                 <option>Uttar Pradesh</option>
